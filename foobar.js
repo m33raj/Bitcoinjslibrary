4fef231ccaa091120e90a255bfaa473760a49429
ashrax <arajbose@gmail.com>
2015-10-23 17:49:05 -0400

    Update foobar.js

--- foobar.js
+++ foobar.js
@@ -1,1 +1,9 @@
-
+var foobar = {
+  base58: require('bs58'),
+  bitcoin: require('bitcoinjs-lib'),
+  ecurve: require('ecurve'),
+  BigInteger: require('bigi'),
+  Buffer: require('buffer')
+}
+ 
+module.exports = foobar
