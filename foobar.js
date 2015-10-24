b501be345cd743adea35968dcc14d864d7d7be6c
ashrax <arajbose@gmail.com>
2015-10-23 21:19:40 -0400

    Delete foobar.js

--- foobar.js
+++
@@ -1,9 +0,0 @@
-var foobar = {
-  base58: require('bs58'),
-  bitcoin: require('bitcoinjs-lib'),
-  ecurve: require('ecurve'),
-  BigInteger: require('bigi'),
-  Buffer: require('buffer')
-}
- 
-module.exports = foobar
